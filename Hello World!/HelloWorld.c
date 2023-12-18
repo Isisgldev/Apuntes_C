73e6278f559e43928f4c2e7d5c8b5d8ab03f8bf7
Isis Gómez López <43893808+Isisgldev@users.noreply.github.com>
2023-12-18 10:55:59 +0100

    Create HelloWorld.c

---
+++ Hello World!/HelloWorld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>  //Librería necesaria que contiene como ejecutar la función printf.
+
+int main () //indica el inicio o punto de partida del código, se ejecutará primero lo que esta entre las llaves.
+{
+  //función que inprime en la consola lo que esta entre paréntesis. 
+  
+  printf("Hello World\n");
+}
